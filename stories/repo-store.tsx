5187a555c27234c1b36b422433fd3ddbefadbe82
Finotelli <carlos.finotelli@outlook.com>
2024-07-28 13:57:25 -0300

    feat/implement zustand (#4)
    * feat/install zustand with console to return test by async-storage
    
    * feat/implement zustand

---
+++ stories/repo-store.tsx
@@ -0,0 +1,60 @@
+import AsyncStorage from "@react-native-async-storage/async-storage";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+import {
+  createItemHelper,
+  updateItemHelper,
+  removeItemHelper,
+  createListHelper,
+  updateListHelper,
+  removeListHelper,
+} from "./helpers/repo-store.helpers";
+import { StateProps, ItemProps, ListProps } from "./repo-store.types";
+
+export const useRepoStore = create(
+  persist<StateProps>(
+    (set) => ({
+      items: [],
+      createItem: (item: ItemProps) =>
+        set((state) => ({
+          items: createItemHelper(state.items, item),
+        })),
+      updateItem: (item: ItemProps) => {
+        set((state) => ({
+          items: updateItemHelper(state.items, item),
+        }));
+      },
+      removeItem: (itemId: string) =>
+        set((state) => ({
+          items: removeItemHelper(state.items, itemId),
+        })),
+      updatedItemsOnDrag: (currentItems: ItemProps[]) =>
+        set({
+          items: currentItems,
+        }),
+
+      lists: [],
+      createList: (list: ListProps) =>
+        set((state) => ({
+          lists: createListHelper(state.lists, list),
+        })),
+      updateList: (list: ListProps) => {
+        set((state) => ({
+          lists: updateListHelper(state.lists, list),
+        }));
+      },
+      removeList: (listId: string) =>
+        set((state) => ({
+          lists: removeListHelper(state.lists, listId),
+        })),
+      updatedListsOnDrag: (currentLists: ListProps[]) =>
+        set({
+          lists: currentLists,
+        }),
+    }),
+    {
+      name: "@planeja-plus",
+      storage: createJSONStorage(() => AsyncStorage),
+    }
+  )
+);
