3b899742ae8e37536fd452bdc059865422ca8e6f
Carlos Finotelli <carlos.finotelli@outlook.com>
2024-08-27 15:15:30 -0300

    refactor: refact home page creating new components to render lists and reminders

--- src/stories/theme/theme-store.tsx
+++ src/stories/theme/theme-store.tsx
@@ -2,11 +2,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 import { ColorSchemeName as DefaultColorSchemeName } from "react-native";
 import { create } from "zustand";
 import { createJSONStorage, persist } from "zustand/middleware";
-
-export interface ThemeStoreProps {
-  colorTheme: "light" | "dark" | null;
-  setColorTheme: (colorTheme: DefaultColorSchemeName) => void;
-}
+import { ThemeStoreProps } from "./theme-store.types";
 
 export const useThemeStore = create(
   persist<ThemeStoreProps>(
